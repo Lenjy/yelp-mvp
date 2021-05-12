f6774e263a5e35fd8ccf8627445a2dd87b5b9d5a
Benjamin Eycken <be.eycken@gmail.com>
2021-05-12 15:26:49 +0200

    exercice 2 done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "Epicure", category: "french", address: "75008 Paris")
+Restaurant.create(name: "macdo", category: "italian", address: "75010 Paris")
+Restaurant.create(name: "Quick", category: "belgian", address: "75015 Paris")
+Restaurant.create(name: "jesus", category: "french", address: "75011 Paris")
+Restaurant.create(name: "Zeus", category: "french", address: "75020 Paris")
